575d0169799eec87b56e1e69850ca8bd2b993b96
welshers <lwelsh27@students.stab.org>
2024-09-25 09:57:39 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,16 +1,1 @@
-function getFlower(){
-  document.getElementById("slider").innerHTML = document.getElementById("price").value  
-  var seasonInput = document.getElementById("season").value;
-var intentionInput = document.getElementById("intention").value;
-var imgOutput = document.getElementById("output").value
-var priceInput = document.getElementById("price").value
 
-var flowerImg;
-} 
-if (seasonInput == "summer" && price > 60 && intentionInput == "romance") {
-    
-}
-document.getElementById("output").innerHTML = outputText;
-document.getElementById("output").value = seasonInput;
-document.getElementById("output").value = intentionInput;
-document.getElementById("outputBox").value = flowerImg;
