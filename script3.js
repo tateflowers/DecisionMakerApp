bf3aa1d08b0860d9fec73733e0714027e2963e29
tateflowers <tflowers28@students.stab.org>
2024-09-25 09:57:50 -0400

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,1 +1,28 @@
+function getFlower(){ 
+  var seasonInput = document.getElementById("season").value;
+var intentionInput = document.getElementById("intention").value;
+var imgOutput;
+var price = document.getElementById("slider").value
 
+
+if (seasonInput == "summer" && price > 60 && intentionInput == "romance") {
+    imgOutput = "csp.flowers/rose.png"
+}
+else if(seasonInput == "summer" && price < 60 && intentionInput == "romance") {
+    imgOutput = "csp.flowers/poppy.png"
+}
+
+
+console.log(imgOutput);
+    document.getElementById("flower").src = imgOutput;
+}
+
+
+function showPrice(){
+    document.getElementById("price").innerHTML = document.getElementById("slider").value
+}
+
+// document.getElementById("output").value = seasonInput;
+// document.getElementById("output").value = intentionInput;
+document.getElementById("outputBox").src = imgOutput;
+// document.getElementById("output").value = priceInput;
